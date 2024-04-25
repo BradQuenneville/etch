22b811634787d79c3492b311103227719a593011
BradQuenneville <quennevillebradley@gmail.com>
2024-04-24 22:46:35 -0700

    Background color changes to black on mouseenter

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const resetButton = document.querySelector('button')
-const container = document.querySelector('.container')
+const resetButton = document.querySelector('button');
+const container = document.querySelector('.container');
 
 // Make Grid
-const gridSize = 16
+const gridSize = 96;
 
-const createGrid = (gridSize) => {
-    for (i = 0; i < gridSize; i++) {
+const createGrid = (boxNum) => {
+    for (i = 0; i < boxNum; i++) {
     const row = document.createElement('div');
-    row.classList.add(grid-row);
+    row.classList.add('grid-row');
 
-    for (j = 0; j < gridSize; j++) {
-    const column = document.createElement('div');
-    row.classList.add(grid-box);
-    row.appendChild(grid-box);
+    for (j = 0; j < boxNum; j++) {
+    const heightWidth = 960 / gridSize
+    const gridBox = document.createElement('div');
+    gridBox.classList.add('grid-box');
+    gridBox.style.width = `${heightWidth}px`;
+    gridBox.style.height = `${heightWidth}px`;
+    gridBox.addEventListener("mouseenter", () => {
+        gridBox.style.backgroundColor = 'black';
+    });
+    row.appendChild(gridBox);
     }
 
-    container.appendChild(grid-row);
+    container.appendChild(row);
     }
 }
+createGrid(gridSize);
+
+//Hover
